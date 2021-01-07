0f73a942025419dd26c8fa121f2477190417c795
Zeuxis Lo <seekstudio@gmail.com>
2021-01-07 15:47:08 +0800

    add user model with manually configure the h2 to fix the datasource url in development

---
+++ scode-backend/src/main/java/im/ggd/scode/model/User.java
@@ -0,0 +1,40 @@
+package im.ggd.scode.model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private Date updatedAt;
+
+}
