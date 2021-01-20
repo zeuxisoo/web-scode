244b16a8bd6272a3ebbdf5f296843006d4058839
Zeuxis Lo <seekstudio@gmail.com>
2021-01-20 21:25:53 +0800

    update test case for create user and check the username is or not exists when create user

--- scode-backend/src/test/java/im/ggd/scode/controller/UserControllerTest.java
+++ scode-backend/src/test/java/im/ggd/scode/controller/UserControllerTest.java
@@ -1,22 +1,30 @@
 package im.ggd.scode.controller;
 
-import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import im.ggd.scode.ScodeApplication;
+import im.ggd.scode.model.User;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = ScodeApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -25,12 +33,44 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User();
+
+        user.setUsername("test");
+        user.setPassword("testtest");
+        user.setEmail("test@test.com");
+    }
+
     @Test
+    @Order(1)
     public void createUser() throws Exception {
-        mvc.perform(post("/user/create"))
-           .andDo(print())
-           .andExpect(status().isOk())
-           .andExpect(content().string(equalTo("user created")));
+        mvc.perform(
+            post("/user/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user))
+        )
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().string("user created"));
+    }
+
+    @Test
+    @Order(2)
+    public void isUsernameExists() throws Exception {
+        mvc.perform(
+            post("/user/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user))
+        )
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().string("Username already exists"));
     }
 
 }
