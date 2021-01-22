547083bfa3704143b27e35d9c6e2db688a1ecb85
Zeuxis Lo <seekstudio@gmail.com>
2021-01-22 16:16:23 +0800

    remove unsupported submit fields in UserRequest object

--- scode-backend/src/main/java/im/ggd/scode/request/UserRequest.java
+++ scode-backend/src/main/java/im/ggd/scode/request/UserRequest.java
@@ -1,21 +1,14 @@
 package im.ggd.scode.request;
 
-import java.util.Date;
-
 import lombok.Data;
 
 @Data
 public class UserRequest {
-
-    private Long id;
 
     private String username;
 
     private String password;
 
     private String email;
-
-    private Date createdAt;
-    private Date updatedAt;
 
 }
