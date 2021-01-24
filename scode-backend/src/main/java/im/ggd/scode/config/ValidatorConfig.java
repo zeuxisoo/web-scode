1466ec1e1c885aa508030fdc28fa2db4848a9ebb
Zeuxis Lo <seekstudio@gmail.com>
2021-01-24 23:20:44 +0800

    fix the auto injection/autowired object is null and do not working on custom validator when add ValidatorConfig

--- scode-backend/src/main/java/im/ggd/scode/config/ValidatorConfig.java
+++ scode-backend/src/main/java/im/ggd/scode/config/ValidatorConfig.java
@@ -5,11 +5,17 @@ import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
 import org.hibernate.validator.HibernateValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.SpringConstraintValidatorFactory;
 
 @Configuration
 public class ValidatorConfig {
+
+    @Autowired
+    private AutowireCapableBeanFactory autowireCapableBeanFactory;
 
     @Bean
     public Validator validator() {
@@ -17,6 +23,9 @@ public class ValidatorConfig {
 
         validatorFactory = Validation.byProvider(HibernateValidator.class)
             .configure()
+            .constraintValidatorFactory(
+                new SpringConstraintValidatorFactory(autowireCapableBeanFactory)
+            )
             .addProperty("hibernate.validator.fail_fast", "false")
             .buildValidatorFactory();
 
