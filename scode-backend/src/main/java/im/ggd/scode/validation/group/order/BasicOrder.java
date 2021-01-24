1624ece4613ac4291cfe848be1b853a65e1d3584
Zeuxis Lo <seekstudio@gmail.com>
2021-01-24 22:18:47 +0800

    add Default group to custom validation BasicOrder group

--- scode-backend/src/main/java/im/ggd/scode/validation/group/order/BasicOrder.java
+++ scode-backend/src/main/java/im/ggd/scode/validation/group/order/BasicOrder.java
@@ -1,12 +1,13 @@
 package im.ggd.scode.validation.group.order;
 
 import javax.validation.GroupSequence;
+import javax.validation.groups.Default;
 
 import im.ggd.scode.validation.group.EmptyGroup;
 import im.ggd.scode.validation.group.ExistsGroup;
 import im.ggd.scode.validation.group.LengthGroup;
 
-@GroupSequence({ EmptyGroup.class, LengthGroup.class, ExistsGroup.class })
+@GroupSequence({ Default.class, EmptyGroup.class, LengthGroup.class, ExistsGroup.class })
 public interface BasicOrder {
 
 }
