6a7d50fc4114d96e69fa4b6eea2f202c476b8c2d
Zeuxis Lo <seekstudio@gmail.com>
2021-01-24 22:18:22 +0800

    enable hibernate validator fast mode to only return first constraint violation occurs

---
+++ scode-backend/src/main/java/im/ggd/scode/config/ValidatorConfig.java
@@ -0,0 +1,26 @@
+package im.ggd.scode.config;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ValidatorConfig {
+
+    @Bean
+    public Validator validator() {
+        ValidatorFactory validatorFactory;
+
+        validatorFactory = Validation.byProvider(HibernateValidator.class)
+            .configure()
+            .addProperty("hibernate.validator.fail_fast", "false")
+            .buildValidatorFactory();
+
+        return validatorFactory.getValidator();
+    }
+
+}
