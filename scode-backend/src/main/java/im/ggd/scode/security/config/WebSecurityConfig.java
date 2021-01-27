be985f55d781f6f3bab8bfe04c7231e573979fbb
Zeuxis Lo <seekstudio@gmail.com>
2021-01-27 21:45:25 +0800

    group all security related file into security directory and make the session work again to protect each page excluded /h2-console, /user/create and form login, logout page

---
+++ scode-backend/src/main/java/im/ggd/scode/security/config/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package im.ggd.scode.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.headers().frameOptions().sameOrigin();
+
+        // http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+            .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/user/create").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+
+        http.exceptionHandling().accessDeniedPage("/login");
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
---
+++ scode-backend/src/main/java/im/ggd/scode/security/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,44 @@
+package im.ggd.scode.security.service.impl;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import im.ggd.scode.model.User;
+import im.ggd.scode.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Cannot not found user by " + username);
+        }
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(username)
+                .password(user.getPassword())
+                // .authorities(new SimpleGrantedAuthority("ADMIN"))
+                .authorities(new ArrayList<SimpleGrantedAuthority>() {{
+                    new SimpleGrantedAuthority("ADMIN");
+                    new SimpleGrantedAuthority("USER");
+                }})
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+
+}
