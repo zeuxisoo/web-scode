3facc2f1386c24f3de0186604c1c88b54d8e6d02
Zeuxis Lo <seekstudio@gmail.com>
2021-01-27 16:02:18 +0800

    make the user's username and email unique and not null by default in the database schema

--- scode-backend/src/main/java/im/ggd/scode/model/User.java
+++ scode-backend/src/main/java/im/ggd/scode/model/User.java
@@ -26,10 +26,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String username;
 
     private String password;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
     @Column(name = "created_at")
