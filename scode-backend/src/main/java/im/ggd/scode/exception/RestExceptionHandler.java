2a48458ceced094ff6a96a9627c0ce3f114e0131
Zeuxis Lo <seekstudio@gmail.com>
2021-01-28 20:50:11 +0800

    handle authentication exception when user provided invalid username or password in sign in action

--- scode-backend/src/main/java/im/ggd/scode/exception/RestExceptionHandler.java
+++ scode-backend/src/main/java/im/ggd/scode/exception/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package im.ggd.scode.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,6 +27,12 @@ public class RestExceptionHandler {
     @ExceptionHandler({ HttpMessageNotReadableException.class })
     public ErrorResponse handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
         return createErrorResponse(false, "No content found in the request body");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({ AuthenticationException.class })
+    public ErrorResponse handleAuthenticationException(AuthenticationException e) {
+        return createErrorResponse(false, "Invalid username / password");
     }
 
     //
