c160de7ec5951b3ba372fd500e07b27de0f1b9ac
Zeuxis Lo <seekstudio@gmail.com>
2021-01-30 20:09:06 +0800

    replace response "user created" message to the created user object when call api.user.create /user/create route

---
+++ scode-backend/src/main/java/im/ggd/scode/dto/transformer/UserTransformer.java
@@ -0,0 +1,29 @@
+package im.ggd.scode.dto.transformer;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import im.ggd.scode.model.UserModel;
+
+public class UserTransformer implements ITransformer {
+
+    @Override
+    public Map<String, Object> transform(Object object) {
+        UserModel userModel = (UserModel) object;
+
+        Map<String, Object> user = Stream.of(new Object[][] {
+            { "username",   userModel.getUsername() },
+            { "email",      userModel.getEmail() },
+            { "created_at", userModel.getCreatedAt() },
+        }).collect(
+            Collectors.toMap(
+                data -> (String) data[0],
+                data -> (Object) data[1]
+            )
+        );
+
+        return user;
+    }
+
+}
