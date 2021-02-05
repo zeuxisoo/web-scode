bfd269ce32fbab89468eb8a06705e824a5508110
Zeuxis Lo <seekstudio@gmail.com>
2021-02-05 21:24:06 +0800

    add BaseTestCase for each test case extends it

---
+++ scode-backend/src/test/java/im/ggd/scode/BaseTestCase.java
@@ -0,0 +1,16 @@
+package im.ggd.scode;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ScodeApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public abstract class BaseTestCase {
+
+}
--- scode-backend/src/test/java/im/ggd/scode/controller/user/CreateTest.java
+++ scode-backend/src/test/java/im/ggd/scode/controller/user/CreateTest.java
@@ -10,27 +10,17 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import im.ggd.scode.ScodeApplication;
+import im.ggd.scode.BaseTestCase;
 import im.ggd.scode.entity.UserEntity;
 import im.ggd.scode.utils.UserUtils;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = ScodeApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-public class CreateTest {
+public class CreateTest extends BaseTestCase {
 
     @Autowired
     private MockMvc mvc;
