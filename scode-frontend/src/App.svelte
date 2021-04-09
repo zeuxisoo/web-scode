481414e548b8be047e6f64ea03abbbd93f632f37
Zeuxis Lo <seekstudio@gmail.com>
2021-04-09 16:10:12 +0800

    add bootstrap framework and config svelte preprocess to support lang="scss" or lang="postcss" in style tag by default

--- scode-frontend/src/App.svelte
+++ scode-frontend/src/App.svelte
@@ -2,10 +2,18 @@
 
 </script>
 
-<style>
+<style lang="postcss" global>
+@import "bootstrap";
 
+body {
+    padding-top: 15px;
+}
 </style>
 
 <div id="app">
-    SCode frontend
+    <div class="container">
+        <div class="header d-flex justify-content-center">
+            <h1>SCode</h1>
+        </div>
+    </div>
 </div>
--- scode-frontend/vite.config.js
+++ scode-frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import svelte from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import svelte from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+    plugins: [
+        svelte({
+            preprocess: sveltePreprocess({
+                defaults: {
+                    style: "postcss"
+                },
+                postcss: true,
+                scss: {
+                    includePaths: ['src', 'node_modules'],
+                },
+            })
+        })
+    ]
+});
