fb0417a53b6f7fc4c8d8aa5c6a66c02afd7711f2
Zeuxis Lo <seekstudio@gmail.com>
2021-04-10 13:18:02 +0800

    add register page layout

--- scode-frontend/src/views/Register.svelte
+++ scode-frontend/src/views/Register.svelte
@@ -1,3 +1,23 @@
 <div id="register">
-    Register page
+    <div class="card">
+        <div class="card-body">
+            <div class="row g-3">
+                <div class="col-12">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="username" placeholder="your username">
+                </div>
+                <div class="col-12">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="username@example.com">
+                </div>
+                <div class="col-12">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="your password">
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-secondary">Sign up</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
