957d2e4091a3577bf8e0c56c96d0ce149d885e7c
Zeuxis Lo <seekstudio@gmail.com>
2021-04-10 20:29:26 +0800

    add checking rule in the login form to validate user input data

--- scode-frontend/src/views/Login.svelte
+++ scode-frontend/src/views/Login.svelte
@@ -1,17 +1,41 @@
+<script>
+import { trimData } from '../utils';
+import Validator from '../utils/validator';
+
+let data = {
+    username: { value: "", ref: null },
+    password: { value: "", ref: null },
+}
+
+function handleSignIn() {
+    data = trimData(data);
+
+    const rules = {
+        'username': 'isNotEmpty',
+        'password': 'isNotEmpty',
+    }
+
+    if (Validator.passes(data, rules)) {
+        console.log(data);
+    }
+}
+</script>
+
 <div id="login">
     <div class="card">
         <div class="card-body">
             <div class="row g-3">
                 <div class="col-12">
                     <label for="username" class="form-label">Username</label>
-                    <input type="text" class="form-control" id="username" placeholder="your username">
+                    <!-- svelte-ignore a11y-autofocus -->
+                    <input type="text" class="form-control" id="username" placeholder="your username" autofocus bind:value={data.username.value} bind:this={data.username.ref}>
                 </div>
                 <div class="col-12">
                     <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="your password">
+                    <input type="password" class="form-control" id="password" placeholder="your password" bind:value={data.password.value} bind:this={data.password.ref}>
                 </div>
                 <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Sign in</button>
+                    <button type="submit" class="btn btn-primary" on:click={handleSignIn}>Sign in</button>
                 </div>
             </div>
         </div>
