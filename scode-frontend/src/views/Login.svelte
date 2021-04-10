2870cdc2ccbc27ee59dfc16a6760e69a80e4d440
Zeuxis Lo <seekstudio@gmail.com>
2021-04-10 13:17:53 +0800

    add login page layout

--- scode-frontend/src/views/Login.svelte
+++ scode-frontend/src/views/Login.svelte
@@ -1,3 +1,19 @@
 <div id="login">
-    Login page
+    <div class="card">
+        <div class="card-body">
+            <div class="row g-3">
+                <div class="col-12">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="username" placeholder="your username">
+                </div>
+                <div class="col-12">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="your password">
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
