145288007a5c558caff1bf0522c25680d5526f8f
Zeuxis Lo <seekstudio@gmail.com>
2021-04-11 14:38:56 +0800

    make each backend exception handler should be response 200 ok status because the response data contain ok state keywords

--- scode-backend/src/main/java/im/ggd/scode/exception/RestExceptionHandler.java
+++ scode-backend/src/main/java/im/ggd/scode/exception/RestExceptionHandler.java
@@ -21,13 +21,13 @@ public class RestExceptionHandler {
     @Autowired
     private ErrorResponse errorResponse;
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler({ MethodArgumentTypeMismatchException.class })
     public ErrorResponse handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException e) {
         return createErrorResponse(false, "The type of argument incorrect");
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler({ MethodArgumentNotValidException.class })
     public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
         String firstErrorMessage = e.getBindingResult().getFieldError().getDefaultMessage();
@@ -37,25 +37,25 @@ public class RestExceptionHandler {
         return errorResponse;
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler({ HttpMessageNotReadableException.class })
     public ErrorResponse handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
         return createErrorResponse(false, "No content found in the request body");
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler({ AuthenticationException.class })
     public ErrorResponse handleAuthenticationException(AuthenticationException e) {
         return createErrorResponse(false, "Invalid username / password");
     }
 
-    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler({ UsernameNotFoundException.class })
     public ErrorResponse handleUsernameNotFoundException(UsernameNotFoundException e) {
         return createErrorResponse(false, "Cannot found user by username");
     }
 
-    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler({ UserNotFoundException.class })
     public ErrorResponse handleUserNotFoundException(UserNotFoundException e) {
         return createErrorResponse(false, "User not found in current status");
