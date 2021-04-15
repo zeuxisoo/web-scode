0612360d5c02bc2c60969a158ec4f9c7e772f375
Zeuxis Lo <seekstudio@gmail.com>
2021-04-15 18:57:38 +0800

    add pre condition in the /article/create route to check the login state

--- scode-frontend/src/App.svelte
+++ scode-frontend/src/App.svelte
@@ -1,6 +1,6 @@
 <script>
 import Router from 'svelte-spa-router';
-import routes from './routes';
+import { createRoutes } from './routes';
 import { createDefaultContext, useDefaultContext } from './context/default.js';
 import gate from './utils/gate';
 import notifier from './utils/notifier';
@@ -10,6 +10,7 @@ createDefaultContext();
 
 // Get the default context
 const defaultContext = useDefaultContext();
+const routes = createRoutes();
 
 // Try to activate authenticated status when token is exists
 gate.reactivate(defaultContext);
--- scode-frontend/src/routes.js
+++ scode-frontend/src/routes.js
@@ -1,17 +1,48 @@
+import { replace } from 'svelte-spa-router';
+import { wrap } from 'svelte-spa-router/wrap';
+import { useDefaultContext } from './context/default';
 import Home from './views/Home.svelte';
 import Register from './views/Register.svelte';
 import Login from './views/Login.svelte';
 import ArticleCreate from './views/article/Create.svelte';
 import NotFound from './views/NotFound.svelte';
 
-const routes = {
-    '/'              : Home,
-    '/register'      : Register,
-    '/login'         : Login,
-    '/article'       : ArticleCreate,
-    '/article/*'     : ArticleCreate,
-    '/article/create': ArticleCreate,
-    '*'              : NotFound,
-};
+function createRoutes() {
+    const defaultContext = useDefaultContext();
 
-export default routes;
+    const ArticleCreateWrap = wrap({
+        component: ArticleCreate,
+        conditions: [
+            detail => {
+                return new Promise((resolve, reject) => {
+                    defaultContext.subscribe(state => {
+                        const isAuthenticated = state.isAuthenticated;
+
+                        if (isAuthenticated) {
+                            resolve(true);
+                        }else{
+                            replace('/');
+                            reject(false);
+                        }
+                    });
+                });
+            }
+        ]
+    });
+
+    const routes = {
+        '/'              : Home,
+        '/register'      : Register,
+        '/login'         : Login,
+        '/article'       : ArticleCreateWrap,
+        '/article/*'     : ArticleCreateWrap,
+        '/article/create': ArticleCreateWrap,
+        '*'              : NotFound,
+    };
+
+    return routes;
+}
+
+export {
+    createRoutes,
+}
